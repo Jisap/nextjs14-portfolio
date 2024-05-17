061415dcb6b18bf0d3a357a6b4141bb0babd25bf
Jorge Matos <jisapjisap@gmail.com>
2024-05-17 13:07:01 +0200

    header

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ import Nav from "./Nav"
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
           <h1 className="text-4xl font-semibold">
             Luke<span className="text-accent">.</span>
@@ -15,7 +15,7 @@ const Header = () => {
         </Link>
 
         {/* desktop nav  & hire me button*/}
-        <div className="hidden xl:flex">
+        <div className="hidden xl:flex items-center gap-8">
           <Nav />
           <Link href="/contact">
             <Button>Hire me</Button>
