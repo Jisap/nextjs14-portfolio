4ab49857951184bbac11dcb372d9a6cf528b604a
Jorge Matos <jisapjisap@gmail.com>
2024-05-17 13:04:45 +0200

    header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import Link from "next/link"
+import { Button } from "./ui/button"
+import Nav from "./Nav"
+
+
 
 const Header = () => {
   return (
-    <header>Header</header>
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto">
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Luke<span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+        {/* desktop nav  & hire me button*/}
+        <div className="hidden xl:flex">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+
+        {/* mobile nav */}
+        <div className="xl:hidden">
+          mobile nav
+        </div>
+      </div>
+    </header>
   )
 }
 
