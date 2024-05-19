345abfc34d0f519f48e4101a8f0bbf6d40b533be
Jorge Matos <jisapjisap@gmail.com>
2024-05-19 16:54:50 +0200

    Home

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,34 @@
-import Image from "next/image";
+import { Button } from '@/components/ui/button'
+import { FiDownload } from "react-icons/fi"
 
-export default function Home() {
+
+
+const Home = () => {
   return (
-    <main>
-      homepage
-    </main>
-  );
+    <section className='full'> 
+      <div className='container mx-auto h-full'>
+        <div className='flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24'>
+          {/* text */}
+          <div className='text-center xl:text-left'>
+            <span className='text-xl'>Software Developer</span>
+            <h1 className='h1 mb-6'>
+              Hello I'm <br/> <span className='text-accent'>Luke Coleman</span>
+            </h1>
+            <p className='max-w-[550px] mb-9 text-white/80'>
+              I excel at crafting elegant digital eperiences and I am 
+              proficient in various programming languages and technologies.
+            </p>
+            {/* btn & socials */}
+            <div>
+              
+            </div>
+          </div>
+          {/* photo */}
+          <didv>photo</didv>
+        </div>
+      </div>
+    </section>
+  )
 }
+
+export default Home
