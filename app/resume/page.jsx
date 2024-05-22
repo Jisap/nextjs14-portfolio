afb3f8eeab170de93178950061fc08e1abec3ef1
Jorge Matos <jisapjisap@gmail.com>
2024-05-22 17:01:27 +0200

    Resume

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+"use client"
+
+import { FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodejs } from 'react-icons/fa'
+import { SiTailwindcss, SiNextdotjs } from 'react-icons/si';
+
+const about = {
+  title: 'About me',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Luke Coleman"
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+40) 321 654 678"
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "12+ Years"
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "American"
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "luke.01@gmail.com"
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Avaliable"
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "English, Spanish"
+    },
+  ]
+}
 
 const Resume = () => {
   return (
-    <div>Resume</div>
+    <div>
+
+    </div>
   )
 }
 
